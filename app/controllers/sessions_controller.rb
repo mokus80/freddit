cc423f41e065fdce1e2c6454a9fbebd28ac7c743
Nicole <mokus80@gmail.com>
2013-06-29 16:43:58 +0200

    fix login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by(email: params[:session][:email].downcase)
+  	user = User.find_by(email: params[:sessions][:email].downcase)
   	sign_in user
   		# if user && user.authenticate(params[:session][:email])
     # #Sign the user in and redirect to the user's show page.
