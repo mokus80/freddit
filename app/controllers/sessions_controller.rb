64be389cb3d52e63d0beee39d4f6219147b54120
queenfrankie <laurawadden@gmail.com>
2013-07-01 17:31:38 +0200

    Improved the user flow and Created difference between signed-in and signed-out users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,12 @@ def create
 	user = User.find_by(email: params[:sessions][:email].downcase)
 
     # puts "!!!email is " + params["sessions"]["email"]
-	sign_in user
-	redirect_to ideas_path
+	if user.nil?
+		redirect_to signup_path
+	else
+		sign_in user
+		redirect_to root_path
+	end
 end
 
 def destroy
